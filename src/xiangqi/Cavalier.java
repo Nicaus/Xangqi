d04e6607695b30948f09361164e6c66a3132a37b
Lemar Andar <lemarandar@outlook.com>
2021-11-30 23:10:30 -0500

    Cavalier est maintenant fixée

--- src/xiangqi/Cavalier.java
+++ src/xiangqi/Cavalier.java
@@ -17,21 +17,17 @@ public class Cavalier extends Piece{
             return true;
 
         if (arrivee.getColonne() >= 0 && arrivee.getColonne() <= 9 && arrivee.getLigne() >= 0 && arrivee.getLigne() <= 8) {
-            if (arrivee.getColonne() == depart.getColonne() + 2) {
-                if (arrivee.getLigne() == depart.getLigne() + 1 || arrivee.getLigne() == depart.getLigne() - 1)
-                    return true;
+            if (arrivee.getColonne() == depart.getColonne() + 2 && arrivee.getLigne() == depart.getLigne() - 1 || arrivee.getColonne() == depart.getColonne() + 2 && arrivee.getLigne() == depart.getLigne() + 1) {
+                return true;
             }
-            else if (arrivee.getColonne() == depart.getColonne() - 2) {
-                if (arrivee.getLigne() == depart.getLigne() + 1 || arrivee.getLigne() == depart.getLigne() - 1)
-                    return true;
+            else if (arrivee.getColonne() == depart.getColonne() - 2 && arrivee.getLigne() == depart.getLigne() - 1 || arrivee.getColonne() == depart.getColonne() - 2 && arrivee.getLigne() == depart.getLigne() + 1) {
+                return true;
             }
-            else if (arrivee.getLigne() == depart.getLigne() + 2) {
-                if (arrivee.getColonne() == depart.getColonne() + 2 || arrivee.getColonne() == depart.getColonne() - 2)
-                    return true;
+            else if (arrivee.getLigne() == depart.getLigne() - 2 && arrivee.getColonne() == depart.getColonne() + 1 || arrivee.getLigne() == depart.getLigne() - 2 && arrivee.getColonne() == depart.getColonne() - 1) {
+                return true;
             }
-            else if (arrivee.getLigne() == depart.getLigne() - 2) {
-                if (arrivee.getColonne() == depart.getColonne() + 2 || arrivee.getColonne() == depart.getColonne() - 2)
-                    return true;
+            else if (arrivee.getLigne() == depart.getLigne() + 2 && arrivee.getColonne() == depart.getColonne() + 1 || arrivee.getLigne() == depart.getLigne() + 2 && arrivee.getColonne() == depart.getColonne() - 1) {
+                return true;
             }
         }
         return false;
